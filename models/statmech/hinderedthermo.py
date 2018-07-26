31b144227f1fb306db5587eaf087c78ebc122fe9
jonlym <jonathanalym@gmail.com>
2018-07-26 14:27:53 -0400

    Added hinderedthermo module. Minor bug correction.

--- models/statmech/hinderedthermo.py
+++ models/statmech/hinderedthermo.py
@@ -1,35 +1,39 @@
 # -*- coding: utf-8 -*-
-"""
-Thermochemistry.models.statmech.idealgasthermo
-Vlachos group code for thermodynamic models.
-Created on Tues Jul 10 12:40:00 2018
-"""
 
 import numpy as np
 from ase import thermochemistry
 from Thermochemistry import constants as c
 from Thermochemistry.models.statmech.heat_capacity import get_CvoR_trans, get_CvoR_vib, get_CvoR_rot
 
-class IdealGasThermo:
+class HinderedThermo:
 	"""
-	Ideal Gas Model. Uses ase.thermochemistry.IdealGasThermo to calculate
-	enthalpy and entropy
+	Hindered translator/hindered rotor model. 
+	Used for surface species where there are 2 translational modes,
+	1 rotational mode perpendicular to the surface, and 3N-3 vibrational modes.
+
+	See further documentation from the ASE implementation. 
+	https://wiki.fysik.dtu.dk/ase/ase/thermochemistry/thermochemistry.html#hindered-translator-hindered-rotor-model
+
 	Attributes
-		model - ase.thermochemistry.IdealGasThermo
-			Ideal Gas Model which takes the following parameters
-				vib_energies - (N,) ndarray
-					Vibrational frequencies in eV
-				geometry - str
-					Geometry of molecule. Supported options:
-						monatomic
-						linear
-						nonlinear
+		model - ase.Thermochemistry.HinderedThermo object
+			The Hindered Thermo object has the following attributes.
+				vib_energies - (3N,) ndarray where N is number of atoms
+					Vibrational energies in eV.
+				trans_barrier_energy - float
+					Translational energy barrier in eV.
+				rot_barrier_energy - float
+					Rotational energy barrier in eV
+				sitedensity - float
+					Site density in cm^-2
+				rotationalminima - int
+					Number of equivalent minima for an adsorbate full rotation
+					e.g. 6 for an adsorbate on an fcc(111) top site
 				potentialenergy - float
-					DFT electronic energy in eV
+					Potential energy in eV
 				atoms - ase.Atoms object
-					Atoms object required for calculating rotational modes
+					Used to calculate mass and inertia
 				symmetrynumber - int
-		            Symmetry number.
+					Symmetry number of adsorbate. 
 		            Reference point group/symmetry number table:
 		            Point group    symmetry number
 		            C1             1
@@ -46,21 +50,23 @@ class IdealGasThermo:
 		            Td             12
 		            Oh             24
 		            See DOI for more details: 10.1007/s00214-007-0328-0
-		        spin - float
-		            Holds the total electronic spin. 
-		            0 for molecules in which all electrons are paired
-		            0.5 for a free radical with a single unpaired electron
-		            1.0 for a triplet with two unpaired electrons, such as O_2.
+				mass - float
+					Mass of adsorbate in amu. If unspecified, uses the atoms object
+				inertia - float
+					Reduced moment of inertia in amu*A^-2. If unspecified, uses atoms object
 	"""
-	def __init__(self, vib_energies, geometry, potentialenergy=0.0, atoms=None, symmetrynumber=None, spin=None, natoms=None):
-		self.model = thermochemistry.IdealGasThermo(
-			vib_energies = vib_energies,
-			geometry = geometry,
-			potentialenergy = potentialenergy,
-			atoms = atoms, 
-			symmetrynumber = symmetrynumber,
-			spin = spin,
-			natoms = natoms)
+	def __init__(self, vib_energies, trans_barrier_energy, rot_barrier_energy, sitedensity, rotationalminima, potentialenergy=0.0, mass=None, inertia=None, atoms=None, symmetrynumber=1):
+		self.model = thermochemistry.HinderedThermo(
+			vib_energies=vib_energies,
+			trans_barrier_energy=trans_barrier_energy,
+			rot_barrier_energy=rot_barrier_energy,
+			sitedensity=sitedensity,
+			rotationalminima=rotationalminima,
+			potentialenergy=potentialenergy,
+			mass=mass,
+			inertia=inertia,
+			atoms=atoms,
+			symmetrynumber=symmetrynumber)
 
 	def get_CpoR(self, Ts):
 		"""
@@ -75,12 +81,7 @@ class IdealGasThermo:
 			float or (N,) ndarray
 				Dimensionless heat capacity (Cp/R)
 		"""
-		CvoR_trans = get_CvoR_trans(degrees=3.)
-		CvoR_vib = get_CvoR_vib(vib_energies=self.model.vib_energies, Ts=Ts)
-		CvoR_rot = get_CvoR_rot(geometry=self.model.geometry)
-		CvoR_to_CpoR = 1.
-		CpoR = CvoR_trans + CvoR_vib + CvoR_rot + CvoR_to_CpoR
-		return CpoR
+		raise NotImplementedError
 
 	def get_HoRT(self, Ts, verbose=False):
 		"""
@@ -98,11 +99,11 @@ class IdealGasThermo:
 		try:
 			iter(Ts)
 		except TypeError:
-			HoRT = self.model.get_enthalpy(temperature=Ts, verbose=verbose)/(c.kb('eV/K') * Ts)
+			HoRT = self.model.get_internal_energy(temperature=Ts, verbose=verbose)/(c.kb('eV/K') * Ts)
 		else:
 			HoRT = np.zeros_like(Ts)
 			for i, T in enumerate(Ts):
-				HoRT[i] = self.model.get_enthalpy(temperature=T, verbose=verbose)/(c.kb('eV/K') * T)
+				HoRT[i] = self.model.get_internal_energy(temperature=T, verbose=verbose)/(c.kb('eV/K') * T)
 		return HoRT
 
 
@@ -124,11 +125,11 @@ class IdealGasThermo:
 		try:
 			iter(Ts)
 		except TypeError:
-			SoR = self.model.get_entropy(temperature=Ts, pressure=P*c.convert_unit(from_='atm', to='Pa'), verbose=verbose)/c.R('eV/K')
+			SoR = self.model.get_entropy(temperature=Ts, verbose=verbose)/c.R('eV/K')
 		else:
 			SoR = np.zeros_like(Ts)
 			for i, T in enumerate(Ts):
-				SoR[i] = self.model.get_entropy(temperature=T, pressure=P*c.convert_unit(from_='atm', to='Pa'), verbose=verbose)/c.R('eV/K')
+				SoR[i] = self.model.get_entropy(temperature=T, verbose=verbose)/c.R('eV/K')
 		return SoR
 
 	def get_GoRT(self, Ts, P=1., verbose=False):
@@ -149,9 +150,9 @@ class IdealGasThermo:
 		try:
 			iter(Ts)
 		except TypeError:
-			GoRT = self.model.get_gibbs_energy(temperature=Ts, pressure=P*c.convert_unit(from_='bar', to='Pa'), verbose=verbose)/(c.kb('eV/K') * Ts)
+			GoRT = self.model.get_helmholtz_energy(temperature=Ts, pressure=P*c.convert_unit(from_='bar', to='Pa'), verbose=verbose)/(c.kb('eV/K') * Ts)
 		else:
 			GoRT = np.zeros_like(Ts)
 			for i, T in enumerate(Ts):
-				GoRT[i] = self.model.get_gibbs_energy(temperature=T, pressure=P*c.convert_unit(from_='bar', to='Pa'), verbose=verbose)/(c.kb('eV/K') * T)
+				GoRT[i] = self.model.get_helmholtz_energy(temperature=T, pressure=P*c.convert_unit(from_='bar', to='Pa'), verbose=verbose)/(c.kb('eV/K') * T)
 		return GoRT
