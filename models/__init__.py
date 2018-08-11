d10bf766553a4816eeffebe15dfa218d7f95d85a
jonlym <jonathanalym@gmail.com>
2018-08-11 14:59:01 -0400

    Redo commit for build

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-PyMuTT.models
-Vlachos group code to models.
-Created on Tues Jul 10 12:40:00 2018
-"""
--- models/statmech/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-PyMuTT.models.statmech
-Vlachos group code for thermodynamic models.
-Created on Fri Jul 7 12:40:00 2018
-"""
