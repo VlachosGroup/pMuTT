aef88e4bcd56ad846a3f89c8c92092355d4f4610
jonlym <jonathanalym@gmail.com>
2018-09-08 10:11:48 -0400

    Added nuclear mode

---
+++ PyMuTT/models/statmech/nucl.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+class IdealNucl:
+    """Nuclear modes. Assumes no change in any chemical reaction and hence
+    does not affect thermodynamic quantities."""
+
+    def get_q(self):
+        """Calculates the partition function
+
+        Returns
+        -------
+            q_nucl : float
+                Nuclear partition function
+        """
+        return 1.
+
+    def get_CvoR(self):
+        """Calculates the dimensionless heat capacity at constant volume
+
+        Returns
+        -------
+            CvoR_nucl : float
+                Nuclear dimensionless heat capacity at constant volume
+        """
+        return 0.
+
+    def get_CpoR(self):
+        """Calculates the dimensionless heat capacity at constant pressure
+
+        Returns
+        -------
+            CpoR_nucl : float
+                Nuclear dimensionless heat capacity at constant pressure
+        """
+        return 0.
+    
+    def get_UoRT(self):
+        """Calculates the imensionless internal energy
+
+        Returns
+        -------
+            UoRT_nucl : float
+                Nuclear dimensionless internal energy
+        """
+        return 0.
+
+    def get_HoRT(self):
+        """Calculates the dimensionless enthalpy
+
+        Returns
+        -------
+            HoRT_nucl : float
+                Nuclear dimensionless enthalpy
+        """
+        return 0.
+
+    def get_SoR(self):
+        """Calculates the dimensionless entropy
+
+        Returns
+        -------
+            SoR_nucl : float
+                Nuclear dimensionless entropy
+        """
+        return 0.
+
+    def get_AoRT(self):
+        """Calculates the dimensionless Helmholtz energy
+
+        Returns
+        -------
+            AoRT_nucl : float
+                Nuclear dimensionless Helmholtz energy
+        """
+        return 0.
+
+    def get_GoRT(self):
+        """Calculates the dimensionless Gibbs energy
+
+        Returns
+        -------
+            GoRT_nucl : float
+                Nuclear dimensionless Gibbs energy
+        """
+        return 0.
---
+++ tests/test_PyMuTT_model_statmech_nucl.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+PyMuTT.test_PyMuTT_model_statmech_nucl
+Tests for PyMuTT module
+"""
+import unittest
+from PyMuTT.models.statmech import nucl
+
+class TestIdealNucl(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.nuclear = nucl.IdealNucl()
+
+    def test_get_q(self):
+        self.assertEqual(self.nuclear.get_q(), 1.)
+
+    def test_get_CvoR(self):
+        self.assertEqual(self.nuclear.get_CvoR(), 0.)
+
+    def test_get_CpoR(self):
+        self.assertEqual(self.nuclear.get_CpoR(), 0.)
+
+    def test_get_UoRT(self):
+        self.assertEqual(self.nuclear.get_UoRT(), 0.)
+
+    def test_get_HoRT(self):
+        self.assertEqual(self.nuclear.get_HoRT(), 0.)
+
+    def test_get_SoR(self):
+        self.assertEqual(self.nuclear.get_SoR(), 0.)
+
+    def test_get_AoRT(self):
+        self.assertEqual(self.nuclear.get_AoRT(), 0.)
+
+    def test_get_GoRT(self):
+        self.assertEqual(self.nuclear.get_GoRT(), 0.)
+
+if __name__ == '__main__':
+    unittest.main()
