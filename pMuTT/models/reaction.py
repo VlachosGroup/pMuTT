Bugfix pre-exponential factor calculation
Corrected temperature and added e^2 for bimolecular reactions. Temporarily switched default for electronic partition function to not calculate until issues with the partition function are corrected.

pMuTT/models/reaction.py
@@ -569,8 +569,8 @@ class Reaction:
             A : float
                 Pre-exponential factor
         """
-        return c.kb('J/K')*T/c.h('J s')\
-            * np.exp(self.get_SoR_act(rev=rev, T=c.T0('K'), **kwargs))
+        return np.e**2*c.kb('J/K')*T/c.h('J s')\
+            * np.exp(self.get_SoR_act(rev=rev, T=T, **kwargs))
 
     @classmethod
     def from_string(cls, reaction_str, species, species_delimiter='+',