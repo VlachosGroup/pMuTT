763348237bd8f5874a1533e37d2f13b08332c320
jonlym <jonathanalym@gmail.com>
2020-05-14 13:36:25 -0400

    Updated version number to 1.2.21dev

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools_info = {
 	'name': 'pmutt',
-	'version': '1.2.20',
+	'version': '1.2.21dev',
 	'author': 'Vlachos Research Group',
 	'author_email': 'vlachos@udel.edu',
 	'description': 'Python Multiscale Thermochemistry Toolbox (pmutt)',
