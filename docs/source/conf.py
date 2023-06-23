394767f75b1de2b57e02c944d9ac694f409b6c95
Gerhard R Wittreich, P.E <wittregr@udel.edu>
2023-06-23 15:53:25 -0400

    Update build files for v1.4.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = '2020, Vlachos Research Group'
 author = 'Vlachos Research Group'
 
 # The short X.Y version
-version = '1.4.4'
+version = '1.4.5'
 # The full version, including alpha/beta/rc tags
-release = '1.4.4'
+release = '1.4.5'
 sphinx_version = sphinx.__display_version__
 
 
